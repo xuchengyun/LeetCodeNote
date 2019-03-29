462b301bcd5356e21ca77f92b63689f8dfc13ac4
xuchengyun <454166448@qq.com>
2019-03-28 20:03:29 -0700

    commit

--- LCQuestions/lc074SearchA2DMatrix/SearchA2DMatrix.java
+++ LCQuestions/lc074SearchA2DMatrix/SearchA2DMatrix.java
@@ -4,15 +4,20 @@ public class SearchA2DMatrix {
     /*
     find row first and find column then
      */
+    //TODO
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0) return false;
 
         int row = matrix.length;
         int column = matrix[0].length;
 
-        for (int i = 0; i < row; i++) {
-            if (target <= )
-        }
 
+        int targetRow = 0;
+        for (int i = 0; i < row; i++) {
+            if (target <= matrix[i][0]) {
+
+            }
+        }
+    return false;
     }
 }
--- LCQuestions/lc957PrisonCellsAfterNDays/PrisonCellsAfterNDays.java
+++ LCQuestions/lc957PrisonCellsAfterNDays/PrisonCellsAfterNDays.java
@@ -1,4 +1,60 @@
 package LCQuestions.lc957PrisonCellsAfterNDays;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PrisonCellsAfterNDays {
+
+    // brute force
+    public int[] prisonAfterNDays(int[] cells, int N) {
+        while (N > 0) {
+            N--;
+            int[] cells2 = new int[8];
+            for (int i = 1; i < 7; ++i)
+                cells2[i] = cells[i - 1] == cells[i + 1] ? 1 : 0;
+            cells = cells2;
+        }
+        return cells;
+    }
+
+    // 2 ^ 6 = 64 states, we have a circle of 64, to reduce time complexity
+    // For each state, we only meet once, so the time complexity is O(2 ^ N), where N is the number of cells
+    public int[] prisonAfterDay1(int[] cells, int N) {
+        if (N == 0) return cells;
+        Map<String, Integer> visited = new HashMap<>();
+        while (N > 0) {
+            visited.put(Arrays.toString(cells), N);
+            int[] cells2 = new int[8];
+            for (int i = 1; i < 7; i++) {
+                cells2[i] = cells[i - 1] == cells[i + 1] ? 1 : 0;
+            }
+            cells = cells2;
+            N--;
+            if (visited.containsKey(Arrays.toString(cells))) {
+                int thatN = visited.get(Arrays.toString(cells));
+                int diff = N - thatN;
+                N %= diff;
+            }
+        }
+        return cells;
+    }
+
+
+    // find the pattern of it, the length of loop can be 1, 7, or 14.
+    public int[] prisonAfterNDays2(int[] cells, int N) {
+        for (N = (N - 1) % 14 + 1; N > 0; --N) {
+            int[] cells2 = new int[8];
+            for (int i = 1; i < 7; ++i)
+                cells2[i] = cells[i - 1] == cells[i + 1] ? 1 : 0;
+            cells = cells2;
+        }
+        return cells;
+    }
+
+    public static void main(String[] args) {
+        PrisonCellsAfterNDays p = new PrisonCellsAfterNDays();
+        int[] cells = new int[]{1,1,0,1,1,0,1,1};
+        int N = 6;
+    }
 }

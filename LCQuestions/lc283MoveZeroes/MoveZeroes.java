c4db02ed39dcd3428bff894b242c29160808fc9e
xuchengyun <454166448@qq.com>
2019-04-19 00:06:48 -0700

    commit

---
+++ LCQuestions/lc283MoveZeroes/MoveZeroes.java
@@ -0,0 +1,21 @@
+package LCQuestions.lc283MoveZeroes;
+
+public class MoveZeroes {
+    public void moveZeros(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                swap(nums, i, j);
+                j++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}

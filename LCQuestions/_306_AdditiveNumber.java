212fc6f616ccb5fdb3ee0df2399434ac3bccdc6c
xuchengyun <454166448@qq.com>
2019-06-15 01:29:45 -0700

    commit

---
+++ LCQuestions/_306_AdditiveNumber.java
@@ -0,0 +1,14 @@
+package LCQuestions.lc306AdditiveNumber;
+
+public class _306_AdditiveNumber {
+    /**
+     * 第一个数范围（n - 1）/2
+     * 第二个数范围q: n - j >= i && n - j >= j - i
+     * @param num
+     * @return
+     */
+    public boolean isAdditiveNumber(String num) {
+        return true;
+    }
+
+}

162a5077832b4b8c726bd74aaf7a938f721bd445
xuchengyun <454166448@qq.com>
2019-06-24 01:27:42 -0700

    commit

---
+++ LCQuestions/_090_SubsetsII.java
@@ -0,0 +1,29 @@
+package LCQuestions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class _090_SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        Arrays.sort(nums);
+        helper(res, new ArrayList<>(), nums, 0);
+        return res;
+    }
+
+    private void helper(List<List<Integer>> res, List<Integer> list, int[] nums, int index) {
+        res.add(new ArrayList<>(list));
+        for (int i = index; i < nums.length; i++) {
+            if (i != index && nums[i] == nums[i] - 1) {
+                continue;
+            }
+            list.add(nums[i]);
+            helper(res, list, nums,i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+
+}
+
--- LCQuestions/_395_LongestSubstringWithAtLeastKRepeatingCharacters.java
+++ LCQuestions/_395_LongestSubstringWithAtLeastKRepeatingCharacters.java
@@ -2,6 +2,7 @@ package LCQuestions;
 
 public class _395_LongestSubstringWithAtLeastKRepeatingCharacters {
     public int longestSubstring(String s, int k) {
-
+        //TODO
+        return 0;
     }
 }

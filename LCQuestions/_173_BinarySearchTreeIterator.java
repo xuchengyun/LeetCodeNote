2164a159b53062d422944813bddedc82b9bfc7da
xuchengyun <454166448@qq.com>
2019-06-29 18:21:31 -0700

    commit

--- LCQuestions/_173_BinarySearchTreeIterator.java
+++ LCQuestions/_173_BinarySearchTreeIterator.java
@@ -10,11 +10,11 @@ public class _173_BinarySearchTreeIterator {
 
     /** @return the next smallest number */
     public int next() {
-
+        return  0;
     }
 
     /** @return whether we have a next smallest number */
     public boolean hasNext() {
-
+        return false;
     }
 }

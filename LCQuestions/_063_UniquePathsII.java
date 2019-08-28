8ce47c5673c723eff34e12f8f57d01403507bd04
xuchengyun <454166448@qq.com>
2019-08-28 09:03:04 -0700

    commit

--- LCQuestions/_063_UniquePathsII.java
+++ LCQuestions/_063_UniquePathsII.java
@@ -1,6 +1,29 @@
 package LCQuestions;
 
 public class _063_UniquePathsII {
+    public int uniquePathsWithObstacles1(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0] == null) {
+            return 0;
+        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        return helper(m, n, new int[m][n]);
+    }
+
+    private int helper(int m, int n, int[][] memo) {
+        if (m < 0 || n < 0 || memo[m][n] == 1) {
+            return 0;
+        }
+        if (m == 0 || n == 0) {
+            return 1;
+        }
+        if (memo[m][n] != 0) {
+            return memo[m][n];
+        }
+        memo[m][n] = helper(m - 1, n, memo) + helper(m, n - 1, memo);
+        return memo[m][n];
+    }
+
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0] == null) {
             return 0;
@@ -31,28 +54,5 @@ public class _063_UniquePathsII {
             }
         }
         return dp[m - 1][n - 1];
-    }
-
-    public int uniquePathsWithObstacles1(int[][] obstacleGrid) {
-        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0] == null) {
-            return 0;
-        }
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        return helper(m, n, new int[m][n]);
-    }
-
-    private int helper(int m, int n, int[][] memo) {
-        if (m < 0 || n < 0 || memo[m][n] == 1) {
-            return 0;
-        }
-        if (m == 0 || n == 0) {
-            return 1;
-        }
-        if (memo[m][n] != 0) {
-            return memo[m][n];
-        }
-        memo[m][n] = helper(m - 1, n, memo) + helper(m, n - 1, memo);
-        return memo[m][n];
     }
 }
---
+++ LCQuestions/_354_RussianDollEnvelopes.java
@@ -0,0 +1,23 @@
+package LCQuestions;
+
+import java.util.Arrays;
+
+public class _354_RussianDollEnvelopes {
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes == null || envelopes.length == 0) return 0;
+        Arrays.sort(envelopes, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        int dp[] = new int[envelopes.length];
+        int len = 0;
+        for(int[] envelope : envelopes){
+            int index = Arrays.binarySearch(dp, 0, len, envelope[1]);
+            if(index < 0) {
+                index = -(index + 1);
+            }
+            dp[index] = envelope[1];
+            if(index == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+}
